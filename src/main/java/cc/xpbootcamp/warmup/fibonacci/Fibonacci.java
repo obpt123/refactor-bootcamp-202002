1aaa5bdd2e3a4f91e44ca76827df1a456acb3468
ypb <obpt@sina.com>
2020-02-15 20:29:57 +0800

    refact some code

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
@@ -9,16 +9,15 @@ public final class Fibonacci {
     }
 
     private static long calcInternal(final int index, final Map<Integer, Long> cacheData) {
+        if (index <= 0) {
+            throw new IllegalArgumentException("The index should not less than 0.");
+        }
         if (cacheData.containsKey(index)) {
             return cacheData.get(index);
-        } else {
-            if(index<=0) {
-                throw new IllegalArgumentException("The index should not less than 0.");
-            }
-            final long value = index <= 2 ? 1 : calcInternal(index - 1, cacheData) + calcInternal(index - 2, cacheData);
-            cacheData.put(index, value);
-            return value;
         }
+        final long value = index <= 2 ? 1 : calcInternal(index - 1, cacheData) + calcInternal(index - 2, cacheData);
+        cacheData.put(index, value);
+        return value;
     }
 
 }
