66a4403b35692a0800028e73483a60bd406295ac
ypb <obpt@sina.com>
2020-02-15 18:09:50 +0800

    add second test case( index =2)

--- src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/FibonacciTest.java
@@ -8,4 +8,8 @@ public class FibonacciTest {
     public void shouldGet1WhenFibonacciCalcAndGivenIndex1() {
         Assert.assertEquals(1, Fibonacci.Calc(1));
     }
+    @Test
+    public void shouldGet1WhenFibonacciCalcAndGivenIndex2(){
+        Assert.assertEquals(1, Fibonacci.Calc(2));
+    }
 }
