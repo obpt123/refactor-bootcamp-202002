0c46e114b524dc2bbd4b8f4342e607f422aa9dc1
ypb <obpt@sina.com>
2020-02-15 19:11:48 +0800

    split printReceipt function

--- src/main/java/cc/xpbootcamp/warmup/cashier/OrderReceipt.java
+++ src/main/java/cc/xpbootcamp/warmup/cashier/OrderReceipt.java
@@ -1,10 +1,10 @@
 package cc.xpbootcamp.warmup.cashier;
 
 /**
- * OrderReceipt prints the details of order including customer name, address, description, quantity,
- * price and amount. It also calculates the sales tax @ 10% and prints as part
- * of order. It computes the total order amount (amount of individual lineItems +
- * total sales tax) and prints it.
+ * OrderReceipt prints the details of order including customer name, address,
+ * description, quantity, price and amount. It also calculates the sales tax @
+ * 10% and prints as part of order. It computes the total order amount (amount
+ * of individual lineItems + total sales tax) and prints it.
  *
  */
 public class OrderReceipt {
@@ -16,19 +16,23 @@ public class OrderReceipt {
 
     public String printReceipt() {
         StringBuilder output = new StringBuilder();
+        writeHeaderToOutput(output);
+        writeListItemToOutput(output);
+        wirteFooterToOutput(output);
+        return output.toString();
+    }
 
+    private void writeHeaderToOutput(StringBuilder output) {
         // print headers
         output.append("======Printing Orders======\n");
 
         // print date, bill no, customer name
-//        output.append("Date - " + order.getDate();
+        // output.append("Date - " + order.getDate();
         output.append(order.getCustomerName());
         output.append(order.getCustomerAddress());
-//        output.append(order.getCustomerLoyaltyNumber());
+    }
 
-        // prints lineItems
-        double totSalesTx = 0d;
-        double tot = 0d;
+    private void writeListItemToOutput(StringBuilder output) {
         for (LineItem lineItem : order.getLineItems()) {
             output.append(lineItem.getDescription());
             output.append('\t');
@@ -38,6 +42,14 @@ public class OrderReceipt {
             output.append('\t');
             output.append(lineItem.totalAmount());
             output.append('\n');
+        }
+    }
+
+    private void wirteFooterToOutput(StringBuilder output) {
+        double totSalesTx = 0d;
+        double tot = 0d;
+        for (LineItem lineItem : order.getLineItems()) {
+
 
             // calculate sales tax @ rate of 10%
             double salesTax = lineItem.totalAmount() * .10;
@@ -46,12 +58,11 @@ public class OrderReceipt {
             // calculate total amount of lineItem = price * quantity + 10 % sales tax
             tot += lineItem.totalAmount() + salesTax;
         }
-
         // prints the state tax
         output.append("Sales Tax").append('\t').append(totSalesTx);
 
         // print total amount
         output.append("Total Amount").append('\t').append(tot);
-        return output.toString();
+
     }
 }
