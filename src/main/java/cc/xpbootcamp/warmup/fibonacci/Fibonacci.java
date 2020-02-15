54ceafb867d68a299e9eec8404ab1186988def0c
ypb <obpt@sina.com>
2020-02-15 18:27:43 +0800

    promote performance

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/Fibonacci.java
@@ -1,7 +1,21 @@
 package cc.xpbootcamp.warmup.fibonacci;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public final class Fibonacci {
-    public static long Calc(int index) {
-        return index <= 2 ? 1 : Calc(index - 1) + Calc(index - 2);
+    public static long Calc(final int index) {
+        return calcInternal(index, new HashMap<>());
     }
+
+    private static long calcInternal(final int index, final Map<Integer, Long> cacheData) {
+        if (cacheData.containsKey(index)) {
+            return cacheData.get(index);
+        } else {
+            final long value = index <= 2 ? 1 : calcInternal(index - 1, cacheData) + calcInternal(index - 2, cacheData);
+            cacheData.put(index, value);
+            return value;
+        }
+    }
+
 }
