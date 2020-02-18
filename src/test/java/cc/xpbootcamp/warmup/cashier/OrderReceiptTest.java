7e5a9a6bce15d16a39952a52026213d61699622c
ypb <obpt@sina.com>
2020-02-18 21:56:12 +0800

    split unittest

--- src/test/java/cc/xpbootcamp/warmup/cashier/OrderReceiptTest.java
+++ src/test/java/cc/xpbootcamp/warmup/cashier/OrderReceiptTest.java
@@ -19,18 +19,19 @@ public class OrderReceiptTest {
 
         String output = receipt.printReceipt();
 
-
         assertThat(output, containsString("Mr X"));
         assertThat(output, containsString("Chicago, 60601"));
     }
 
     @Test
-    public void shouldPrintLineItemAndSalesTaxInformation() {
-        List<LineItem> lineItems = new ArrayList<LineItem>() {{
-            add(new LineItem("milk", 10.0, 2));
-            add(new LineItem("biscuits", 5.0, 5));
-            add(new LineItem("chocolate", 20.0, 1));
-        }};
+    public void shouldPrintLineItemsInformation() {
+        List<LineItem> lineItems = new ArrayList<LineItem>() {
+            {
+                add(new LineItem("milk", 10.0, 2));
+                add(new LineItem("biscuits", 5.0, 5));
+                add(new LineItem("chocolate", 20.0, 1));
+            }
+        };
         OrderReceipt receipt = new OrderReceipt(new Order(null, null, LocalDate.now(), lineItems));
 
         String output = receipt.printReceipt();
@@ -38,12 +39,28 @@ public class OrderReceiptTest {
         assertThat(output, containsString("milk\t10.0\t2\t20.0\n"));
         assertThat(output, containsString("biscuits\t5.0\t5\t25.0\n"));
         assertThat(output, containsString("chocolate\t20.0\t1\t20.0\n"));
+    }
+
+    @Test
+    public void shouldPrintSalesAndTaxInformation() {
+
+        List<LineItem> lineItems = new ArrayList<LineItem>() {
+            {
+                add(new LineItem("milk", 10.0, 2));
+                add(new LineItem("biscuits", 5.0, 5));
+                add(new LineItem("chocolate", 20.0, 1));
+            }
+        };
+        OrderReceipt receipt = new OrderReceipt(new Order(null, null, LocalDate.now(), lineItems));
+
+        String output = receipt.printReceipt();
+
         assertThat(output, containsString("Sales Tax\t6.5"));
         assertThat(output, containsString("Total Amount\t71.5"));
     }
 
     @Test
-    public void shouldPrintAdInformationOnOrder(){
+    public void shouldPrintAdInformationOnOrder() {
         Order order = new Order("Mr X", "Chicago, 60601", LocalDate.now(), new ArrayList<LineItem>());
         OrderReceipt receipt = new OrderReceipt(order);
 
@@ -52,11 +69,12 @@ public class OrderReceiptTest {
     }
 
     @Test
-    public void shouldPrintDateInfomationOnOrder(){
+    public void shouldPrintDateInfomationOnOrder() {
         Order order = new Order("Mr X", "Chicago, 60601", LocalDate.of(2020, 2, 17), new ArrayList<>());
         OrderReceipt receipt = new OrderReceipt(order);
-        
+
         String output = receipt.printReceipt();
         assertThat(output, containsString("2020年2月17日, 星期一"));
     }
+
 }
