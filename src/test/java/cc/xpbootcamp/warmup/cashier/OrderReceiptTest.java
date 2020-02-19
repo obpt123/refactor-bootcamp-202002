8c3f1eac72d94621511a53fe413998f599f7a465
ypb <obpt@sina.com>
2020-02-19 12:56:23 +0800

    remove unuse test case

--- src/test/java/cc/xpbootcamp/warmup/cashier/OrderReceiptTest.java
+++ src/test/java/cc/xpbootcamp/warmup/cashier/OrderReceiptTest.java
@@ -6,75 +6,22 @@ import java.util.List;
 
 import org.junit.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.assertEquals;
 
 public class OrderReceiptTest {
+    static final List<LineItem> DefaultLineItems = new ArrayList<LineItem>() {
+        private static final long serialVersionUID = 1L;
+        {
+            add(new LineItem("巧克力", 21.5, 2));
+            add(new LineItem("小白菜", 10.0, 1));
+        }
+    };
 
     @Test
-    public void shouldHasLineItemsInformationWhenPrintOrder() {
-        List<LineItem> lineItems = new ArrayList<LineItem>() {
-            {
-                add(new LineItem("milk", 10.0, 2));
-                add(new LineItem("biscuits", 5.0, 5));
-                add(new LineItem("chocolate", 20.0, 1));
-            }
-        };
-        OrderReceipt receipt = new OrderReceipt(new Order(null, null, LocalDate.of(2020, 2, 17), lineItems));
-
-        String output = receipt.printReceipt();
-
-        assertThat(output, containsString("milk, 10.00×2, 20.00\n"));
-        assertThat(output, containsString("biscuits, 5.00×5, 25.00\n"));
-        assertThat(output, containsString("chocolate, 20.00×1, 20.00\n"));
-    }
-
-    @Test
-    public void shouldHasSalesAndTaxInformationWhenPrintOrder() {
-        List<LineItem> lineItems = new ArrayList<LineItem>() {
-            {
-                add(new LineItem("milk", 10.0, 2));
-                add(new LineItem("biscuits", 5.0, 5));
-                add(new LineItem("chocolate", 20.0, 1));
-            }
-        };
-        OrderReceipt receipt = new OrderReceipt(new Order(null, null, LocalDate.of(2020, 2, 17), lineItems));
-
-        String output = receipt.printReceipt();
-
-        assertThat(output, containsString("税额: 6.50"));
-        assertThat(output, containsString("总价: 71.50"));
-    }
-
-    @Test
-    public void shouldHasAdInformationWhenPrintOrder() {
-        Order order = new Order("Mr X", "Chicago, 60601", LocalDate.of(2020, 2, 17), new ArrayList<LineItem>());
-        OrderReceipt receipt = new OrderReceipt(order);
-
-        String output = receipt.printReceipt();
-        assertThat(output, containsString("======老王超市，值得信赖======"));
-    }
-
-    @Test
-    public void shouldHasDateInfomationWhenPrintOrder() {
-        Order order = new Order("Mr X", "Chicago, 60601", LocalDate.of(2020, 2, 17), new ArrayList<>());
-        OrderReceipt receipt = new OrderReceipt(order);
-
-        String output = receipt.printReceipt();
-        assertThat(output, containsString("2020年2月17日, 星期一"));
-    }
-
-    @Test
-    public void shouldNoDiscountWhenPrintOrderGivenOrderDateIsNotPreferentialDay() {
-        List<LineItem> lineItems = new ArrayList<LineItem>() {
-            {
-                add(new LineItem("巧克力", 21.5, 2));
-                add(new LineItem("小白菜", 10.0, 1));
-            }
-        };
-        OrderReceipt receipt = new OrderReceipt(new Order(null, null, LocalDate.of(2020, 2, 17), lineItems));
-
+    public void shouldNoDiscountWhenPrintOrderGivenOrderDateIsNotWednesday() {
+        // 2020-02-17 is monday
+        LocalDate orderDate = LocalDate.parse("2020-02-17");
+        OrderReceipt receipt = createOrderReceiptWithDefaultLineItems(orderDate);
         String expected = "======老王超市，值得信赖======\n" +
                           "\n" +
                           "2020年2月17日, 星期一\n" +
@@ -88,31 +35,10 @@ public class OrderReceiptTest {
     }
 
     @Test
-    public void shouldHas98PercentDiscountWhenPrintOrderGivenOrderDateIsWednesday(){
-        List<LineItem> lineItems = new ArrayList<LineItem>() {
-            {
-                add(new LineItem("巧克力", 21.5, 2));
-                add(new LineItem("小白菜", 10.0, 1));
-            }
-        };
-        // 2020-2-19 is wednesday
-        OrderReceipt receipt = new OrderReceipt(new Order(null, null, LocalDate.of(2020, 2, 19), lineItems));
-        String output = receipt.printReceipt();
-
-        assertThat(output, containsString("税额: 5.30"));
-        assertThat(output, containsString("折扣: 1.17"));
-        assertThat(output, containsString("总价: 57.13"));
-    }
-    @Test
     public void shouldGetExpectInfomationWhenPrintOrderGivenOrderDateIsWednesday() {
-        List<LineItem> lineItems = new ArrayList<LineItem>() {
-            {
-                add(new LineItem("巧克力", 21.5, 2));
-                add(new LineItem("小白菜", 10.0, 1));
-            }
-        };
-        OrderReceipt receipt = new OrderReceipt(new Order(null, null, LocalDate.of(2020, 2, 19), lineItems));
-
+        // 2020-02-19 is wednesday
+        LocalDate orderDate = LocalDate.parse("2020-02-19");
+        OrderReceipt receipt = createOrderReceiptWithDefaultLineItems(orderDate);
         String expected = "======老王超市，值得信赖======\n" +
                           "\n" +
                           "2020年2月19日, 星期三\n" +
@@ -125,4 +51,10 @@ public class OrderReceiptTest {
                           "总价: 57.13\n";
         assertEquals(expected, receipt.printReceipt());
     }
+
+    private OrderReceipt createOrderReceiptWithDefaultLineItems(LocalDate orderDate) {
+        Order order = new Order("Fake Name", "Fake Address", orderDate, DefaultLineItems);
+        return new OrderReceipt(order);
+    }
+
 }
